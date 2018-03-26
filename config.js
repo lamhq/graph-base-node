39eaa13af371e20303cad069d8dcbc520bb1a1b5
Lam <hqlam.bt@gmail.com>
2018-03-26 15:20:41 +0700

    Update environment and Dockerfile

--- config.js
+++ config.js
@@ -2,11 +2,12 @@ const path = require('path')
 
 var config = {
   port: process.env.PORT || 3000,
+  webUrl: process.env.WEB_URL,
   db: {
     uri: process.env.DB_URI,
     debug: process.env.MONGOOSE_DEBUG==='true'
   },
-  appName: 'Care Interchange',
+  appName: 'React Blog',
   appSecret: 'react blog secret',
   logPath: process.env.LOG_PATH || path.resolve(__dirname, 'logs'),
   sentryDns: process.env.SENTRY_DNS || false,
@@ -20,6 +21,13 @@ var config = {
       },
     },
     autoEmail: 'noreply@careinterchange.com',
+  },
+  accessTokenLifeTime: '3h',
+  s3Config: {
+    accessKeyId: 'AKIAJZYDLKBI7KI7ZF6Q',
+    secretAccessKey: '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',
+    bucket: 'lamhq',
+    region: 'ap-southeast-1',
   }
 }
 
