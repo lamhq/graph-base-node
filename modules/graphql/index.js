381de27e9297cd73a65af3c1aa218cec5c0916c4
Lam <hqlam.bt@gmail.com>
2018-09-03 09:04:32 +0700

    Add graphql demo code

--- modules/graphql/index.js
+++ modules/graphql/index.js
@@ -12,21 +12,39 @@ type Query {
 type Post {
   id: ID!,
   title: String!,
-  content: String
+  content: String,
+  publishedAt: String
 }
 `);
 
 const Post = require('../app/models/post');
 
+class MyPost {
+  constructor(post) {
+    this.post = post;
+  }
+
+  id() {
+    return this.post._id.toString();
+  }
+
+  title() {
+    return this.post.title;
+  }
+
+  publishedAt() {
+    const d = new Date(this.post.createdAt);
+    return d.toISOString();
+  }
+}
+
 // The root provides a resolver function for each API endpoint
 const resolver = {
   hello: () => 'Hello world!',
   posts: () => Post.find().sort({ _id: -1 }),
-  post: args => Post.findById(args.id),
-  Post: {
-    id: () => Math.random() * 10,
-    name: () => 'test',
-    content: () => 'content',
+  post: async (args) => {
+    const item = await Post.findById(args.id);
+    return new MyPost(item.toObject());
   },
 };
 
