ca8a82f965447f6256408797c21c37075b20d3b4
Lam <hqlam.bt@gmail.com>
2018-09-16 12:17:27 +0700

    Post crud

--- src/app/admin/post/resolvers/mutation.js
+++ src/app/admin/post/resolvers/mutation.js
@@ -3,9 +3,9 @@ const { validatePost } = require('../helpers');
 
 async function adminAddPost(category, { data }, { db }) {
   const post = new db.models.Post();
-  const errors = validatePost(data);
-  if (errors) {
-    throw new UserInputError('Please correct your inputs', errors);
+  const inputErrors = validatePost(data);
+  if (inputErrors) {
+    throw new UserInputError('Please correct your inputs', { inputErrors });
   }
 
   post.title = data.title;
@@ -17,14 +17,14 @@ async function adminAddPost(category, { data }, { db }) {
 }
 
 async function adminUpdatePost(category, { id, data }, { db }) {
-  const post = db.models.Post.findById(id);
+  const post = await db.models.Post.findById(id);
   if (!post) {
     throw new UserInputError('Post not found');
   }
 
-  const errors = validatePost(data);
-  if (errors) {
-    throw new UserInputError('Please correct your inputs', errors);
+  const inputErrors = validatePost(data);
+  if (inputErrors) {
+    throw new UserInputError('Please correct your inputs', { inputErrors });
   }
 
   post.title = data.title;
@@ -35,7 +35,18 @@ async function adminUpdatePost(category, { id, data }, { db }) {
   return post;
 }
 
+async function adminDeletePost(category, { id }, { db }) {
+  const post = await db.models.Post.findById(id);
+  if (!post) {
+    throw new UserInputError('Post not found');
+  }
+
+  await post.remove();
+  return post;
+}
+
 module.exports = {
   adminAddPost,
   adminUpdatePost,
+  adminDeletePost,
 };
